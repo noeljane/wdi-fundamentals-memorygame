ffd9c47318adf462f7f42d0b08493728988235e4
Noel Jane Bourg <noelbourg@Noels-MacBook-Pro.local>
2017-11-14 18:13:55 -0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,21 @@
+//check to make sure JS is running
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log ("User flipped " + cardOne);
-console.log ("User flipped " + cardThree);
+
+
+var cards = ["queen","queen","king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " +cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardOne === cardTwo) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
